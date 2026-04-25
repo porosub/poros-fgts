3f060614f6f800b0560a1d1eb8c2c6520b811cde
john bloodborne <offse7@localhost.localdomain>
2026-04-25 21:42:48 +0700

    Added Express.js and listener to index.ts Feat: Added Express.js Feat: Added listener to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("App is running");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
