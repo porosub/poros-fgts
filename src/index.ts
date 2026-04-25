a00c47a21617af4188730d4fa8d2e9676a7982c3
john bloodborne <offse7@localhost.localdomain>
2026-04-25 22:09:55 +0700

    Fixed Nginx interaction with App file Feat: Decided on use of ESM over Common.js Fixed: Temporary app listener now imports type correctly Fixed: Nginx now listens to app instead of localhost Fixed: Added depends-on clause to Nginx in docker-compose.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express from 'express';
+import type { Request, Response } from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("OK");
 });
 
