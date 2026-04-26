338c2d199f202f8be920e81d6583a73a26336568
john bloodborne <offse7@localhost.localdomain>
2026-04-26 14:32:14 +0700

    Added Healthcheck
    Feat: added healthcheck

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,16 @@
-import express from 'express';
-import type { Request, Response } from 'express';
+import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("OK");
+app.get("/api/health", (req: Request, res: Response) => {
+  res.json({ status: "ok" });
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello World from Back Tier" });
 });
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+  console.log("API running on port 3000");
 });
